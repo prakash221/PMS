b53e0a571045f60db986a9ee4d64b191248d0e7d
prakash221 <prakash.psc221@gmail.com>
2020-12-26 14:01:26 +0545

    Update index.php
    Prakash Edition

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>HK</title>
+	<title>Prakash</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	
 </head>
 <body>
-
+<h1>Bhayo mero aba git le kaam garyo</h1>
   <div class="form-group pan">
     <label for="ird">PAN number</label>
     <input type="number" class="form-control" id="ird" >    
